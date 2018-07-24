1685434c591f73a9f3d22fc8da21a2b9b8277747
Stanley Louis <stanlouis@msn.com>
2018-07-24 11:26:07 -0400

    MERN setup

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/gweb"
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.log("Could not connect to MongoDB", err));
+
+// Check nyt_react_search project
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
