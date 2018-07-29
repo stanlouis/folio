d16433a11197b2dffdd2804fc222edde5a632925
Stanley Louis <stanlouis@msn.com>
2018-07-29 13:27:27 -0400

    Add user routes

--- server.js
+++ server.js
@@ -1,24 +1,91 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("./services/passport");
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
 const app = express();
 
-require("./routes/authRoutes")(app);
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/gweb")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.log("Could not connect to MongoDB", err));
+require('./routes/authRoutes')(app);
 
-// Check nyt_react_search project
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+mongoose
+  .connect(config.DATABASE)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect to MongoDB', err));
+
+const { User } = require('./models/user');
+const { auth } = require('./middleware/auth');
+
+// GET routes //
+app.get('/api/auth', auth, (req, res) => {
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname
+  });
+});
+
+app.get('/api/logout', auth, (req, res) => {
+  req.user.deleteToken(req.token, (err, user) => {
+    if (err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+});
+
+// POST routes //
+app.post('/api/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err: err });
+    res.status(200).json({
+      success: true,
+      user: doc
+    });
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        isAuth: false,
+        message: 'Auth failed, email not found'
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          isAuth: false,
+          message: 'Wrong password'
+        });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie('auth', user.token).json({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        });
+      });
+    });
+  });
+});
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+
+app.listen(PORT, () => {
+  console.log(`Server running at port:${PORT}`);
+});
